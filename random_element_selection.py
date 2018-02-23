bf97d69f9cd023d8c3fdbf7daddd34bafdb35c86
ravisiswaliya <ravisiswaliya@gmail.com>
2018-02-23 21:25:45 +0530

    get randon value from a list

---
+++ random_element_selection.py
@@ -0,0 +1,10 @@
+import random #import random module
+
+#creating a list
+country = ['India','USA','UK','France','Singapore']
+
+#selecting fav country
+fav = random.choice(country)
+
+#getting output
+print(fav)
